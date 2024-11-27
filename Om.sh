fa828094e7b753a42762fa3353e94f12a03c2d8c
adiz777 <az23abc@herts.ac.uk>
2024-11-27 16:24:56 +0000

    Updated changes

--- Om.sh
+++ Om.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
 
 # Tool Name: OM
-# Description: A minimalist reconnaissance tool for Kali Linux.
-# Created by Adiz777
+# Description: A comprehensive reconnaissance tool for Kali Linux.
+# Created by Adiz777 (Enhanced by Gemini Advanced)
+
+# --- Configuration ---
+# (You can customize these settings)
+
+# Output directory
+output_dir="om_results"
+
+# Wordlists
+common_wordlist="/usr/share/wordlists/dirb/common.txt"
+medium_wordlist="/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt"
+small_wordlist="/usr/share/wordlists/dirbuster/directory-list-2.3-small.txt"
 
 # --- Functions ---
 
 function banner() {
   echo -e "\e[1;32m
-                         
-     OOOOOOOOO                    
-   OO:::::::::OO                  
-  OO:::::::::::::OO                
- O:::::::OOO:::::::O               
- O::::::O   O::::::O   mmmmmmm    mmmmmmm    
- O:::::O     O:::::O mm:::::::m  m:::::::mm 
- O:::::O     O:::::Om::::::::::mm::::::::::m
- O:::::O     O:::::Om::::::::::::::::::::::m
- O:::::O     O:::::Om:::::mmm::::::mmm:::::m
- O:::::O     O:::::Om::::m   m::::m   m::::m
- O:::::O     O:::::Om::::m   m::::m   m::::m
- O::::::O   O::::::Om::::m   m::::m   m::::m
- O:::::::OOO:::::::Om::::m   m::::m   m::::m
-  OO:::::::::::::OO m::::m   m::::m   m::::m
-   OO:::::::::OO   m::::m   m::::m   m::::m
+                        
+     OOOOOOOOO                     
+  OO:::::::::OO                     
+ OO:::::::::::::OO                    
+O:::::::OOO:::::::O                   
+O::::::O   O::::::O   mmmmmmm   mmmmmmm    
+O:::::O     O:::::O mm:::::::m m:::::::mm 
+O:::::O     O:::::Om::::::::::mm::::::::::m
+O:::::O     O:::::Om::::::::::::::::::::::m
+O:::::O     O:::::Om:::::mmm::::::mmm:::::m
+O:::::O     O:::::Om::::m   m::::m   m::::m
+O:::::O     O:::::Om::::m   m::::m   m::::m
+O:::::O     O:::::Om::::m   m::::m   m::::m
+O::::::O   O::::::Om::::m   m::::m   m::::m
+O:::::::OOO:::::::Om::::m   m::::m   m::::m
+ OO:::::::::::::OO m::::m   m::::m   m::::m
+  OO:::::::::OO   m::::m   m::::m   m::::m
      OOOOOOOOO     mmmmmm   mmmmmm   mmmmmm
-                         
-                         
+                        
+                        
 \e[0m"
 }
 
@@ -49,7 +61,7 @@ function update_system() {
 }
 
 function check_tools() {
-  tools=(nmap netdiscover zenmap masscan dnsrecon dig host fierce whatweb nikto dirb wpscan theharvester maltego recon-ng gobuster enum4linux)
+  tools=(nmap masscan sublist3r assetfinder amass dnsrecon dig host fierce whatweb nikto dirb gobuster wpscan theharvester enum4linux feroxbuster nuclei)
   for tool in "${tools[@]}"; do
     if ! command -v "$tool" &> /dev/null; then
       echo -e "\e[1;33m$tool not found. Installing in background...\e[0m"
@@ -68,9 +80,9 @@ function get_target() {
 }
 
 function create_directories() {
-  mkdir -p "om_results"
+  mkdir -p "$output_dir"
   for tool in "${tools[@]}"; do
-    mkdir -p "om_results/$tool/$target"
+    mkdir -p "$output_dir/$tool/$target"
   done
 }
 
@@ -81,113 +93,150 @@ function get_recon_level() {
   echo "$level"
 }
 
-function run_scans() {
-  echo -e "\e[1;34mRunning reconnaissance scans...\e[0m"
-  recon_level=$(get_recon_level)
+function run_subdomain_enumeration() {
+  echo -e "\e[1;34mEnumerating subdomains...\e[0m"
+  sublist3r -d "$target" -o "$output_dir/sublist3r/$target/subdomains.txt" &
+  assetfinder --subs-only "$target" > "$output_dir/assetfinder/$target/subdomains.txt" &
+  amass enum -d "$target" -o "$output_dir/amass/$target/subdomains.txt" &
+  echo "Subdomain enumeration is running in the background..."
+}
 
-  # Nmap
+function run_nmap_scan() {
+  echo -e "\e[1;34mRunning Nmap scan...\e[0m"
   case $recon_level in
     low) nmap_options="-sT -sV" ;;
     medium) nmap_options="-A" ;;
     high) nmap_options="-A -p-" ;;
   esac
-  nmap $nmap_options "$target" -oN "om_results/nmap/$target/nmap_scan.txt" &
-  echo "nmap_$recon_level is running in the background..."
+  nmap $nmap_options "$target" -oN "$output_dir/nmap/$target/nmap_scan.txt" &
+  echo "Nmap scan is running in the background..."
+}
 
-  # Netdiscover
-  case $recon_level in
-    low) netdiscover_options="-r $target/24 -P" ;;
-    medium) netdiscover_options="-r $target/24" ;;
-    high) netdiscover_options="-r $target/24 -p" ;;
-  esac
-  netdiscover $netdiscover_options -oN "om_results/netdiscover/$target/netdiscover_scan.txt" &
-  echo "netdiscover_$recon_level is running in the background..."
-
-  # Masscan
+function run_masscan_scan() {
+  echo -e "\e[1;34mRunning Masscan scan...\e[0m"
   case $recon_level in
     low) masscan_options="-p80,443" ;;
     medium) masscan_options="-p1-1000" ;;
     high) masscan_options="-p1-65535" ;;
   esac
-  masscan $masscan_options "$target" -oG "om_results/masscan/$target/masscan_scan.txt" &
-  echo "masscan_$recon_level is running in the background..."
+  masscan $masscan_options "$target" -oG "$output_dir/masscan/$target/masscan_scan.txt" &
+  echo "Masscan scan is running in the background..."
+}
 
-  # Dnsrecon
+function run_dns_enumeration() {
+  echo -e "\e[1;34mRunning DNS enumeration...\e[0m"
   case $recon_level in
     low) dnsrecon_options="-d $target" ;;
     medium) dnsrecon_options="-d $target -t std,srv,axfr" ;;
     high) dnsrecon_options="-d $target -t std,srv,axfr,mx,soa,ns" ;;
   esac
-  dnsrecon $dnsrecon_options -o "om_results/dnsrecon/$target/dnsrecon_scan.xml" &
-  echo "dnsrecon_$recon_level is running in the background..."
+  dnsrecon $dnsrecon_options -o "$output_dir/dnsrecon/$target/dnsrecon_scan.xml" &
+  dig "$target" ANY > "$output_dir/dig/$target/dig_scan.txt" &
+  host -t ns "$target" > "$output_dir/host/$target/host_scan.txt" &
+  echo "DNS enumeration is running in the background..."
+}
 
-  # Dig
-  dig "$target" ANY > "om_results/dig/$target/dig_scan.txt" &
-  echo "dig is running in the background..."
-
-  # Host
-  host -t ns "$target" > "om_results/host/$target/host_scan.txt" &
-  echo "host is running in the background..."
-
-  # Fierce
+function run_fierce_scan() {
+  echo -e "\e[1;34mRunning Fierce scan...\e[0m"
   case $recon_level in
     low) fierce_options="-dns $target" ;;
     medium) fierce_options="-dns $target -wide" ;;
     high) fierce_options="-dns $target -wide -connect" ;;
   esac
-  fierce $fierce_options > "om_results/fierce/$target/fierce_scan.txt" &
-  echo "fierce_$recon_level is running in the background..."
+  fierce $fierce_options > "$output_dir/fierce/$target/fierce_scan.txt" &
+  echo "Fierce scan is running in the background..."
+}
 
-  # WhatWeb
-  whatweb "$target" > "om_results/whatweb/$target/whatweb_scan.txt" &
-  echo "whatweb is running in the background..."
+function run_web_server_analysis() {
+  echo -e "\e[1;34mRunning web server analysis...\e[0m"
+  whatweb "$target" > "$output_dir/whatweb/$target/whatweb_scan.txt" &
 
-  # Nikto
   case $recon_level in
     low) nikto_options="-h $target" ;;
     medium) nikto_options="-h $target -evasion 1" ;;
     high) nikto_options="-h $target -evasion 4" ;;
   esac
-  nikto $nikto_options -o "om_results/nikto/$target/nikto_scan.txt" &
-  echo "nikto_$recon_level is running in the background..."
+  nikto $nikto_options -o "$output_dir/nikto/$target/nikto_scan.txt" &
+  echo "Web server analysis is running in the background..."
+}
 
-  # Dirb
-  dirb http://"$target" /usr/share/wordlists/dirb/common.txt -o "om_results/dirb/$target/dirb_scan.txt" &
-  echo "dirb is running in the background..."
-
-  # Gobuster
+function run_directory_bruteforcing() {
+  echo -e "\e[1;34mRunning directory bruteforcing...\e[0m"
   case $recon_level in
-    low) gobuster_options="-u http://$target -w /usr/share/wordlists/dirb/common.txt" ;;
-    medium) gobuster_options="-u http://$target -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt" ;;
-    high) gobuster_options="-u http://$target -w /usr/share/wordlists/dirbuster/directory-list-2.3-small.txt -x php,html,txt,js" ;;
+    low) 
+      gobuster_options="-u http://$target -w $common_wordlist" 
+      feroxbuster_options="-u http://$target -w $common_wordlist" 
+      ;;
+    medium) 
+      gobuster_options="-u http://$target -w $medium_wordlist" 
+      feroxbuster_options="-u http://$target -w $medium_wordlist" 
+      ;;
+    high) 
+      gobuster_options="-u http://$target -w $small_wordlist -x php,html,txt,js" 
+      feroxbuster_options="-u http://$target -w $small_wordlist -x php,html,txt,js" 
+      ;;
   esac
-  gobuster dir $gobuster_options -o "om_results/gobuster/$target/gobuster_scan.txt" &
-  echo "gobuster_$recon_level is running in the background..."
+  gobuster dir $gobuster_options -o "$output_dir/gobuster/$target/gobuster_scan.txt" &
+  feroxbuster $feroxbuster_options -o "$output_dir/feroxbuster/$target/feroxbuster_scan.txt" &
+  echo "Directory bruteforcing is running in the background..."
+}
 
-  # WPScan
+function run_wpscan() {
+  echo -e "\e[1;34mRunning WPScan...\e[0m"
   case $recon_level in
     low) wpscan_options="--url $target" ;;
     medium) wpscan_options="--url $target --enumerate u" ;;
     high) wpscan_options="--url $target --enumerate u,p,t" ;;
   esac
-  wpscan $wpscan_options -o "om_results/wpscan/$target/wpscan_scan.json" &
-  echo "wpscan_$recon_level is running in the background..."
+  wpscan $wpscan_options -o "$output_dir/wpscan/$target/wpscan_scan.json" &
+  echo "WPScan is running in the background..."
+}
 
-  # TheHarvester
+function run_theharvester() {
+  echo -e "\e[1;34mRunning TheHarvester...\e[0m"
   case $recon_level in
     low) theharvester_options="-d $target -l 100 -b all" ;;
     medium) theharvester_options="-d $target -l 500 -b all" ;;
     high) theharvester_options="-d $target -l 1000 -b all" ;;
   esac
-  theharvester $theharvester_options > "om_results/theharvester/$target/theharvester_scan.txt" &
-  echo "theharvester_$recon_level is running in the background..."
+  theharvester $theharvester_options > "$output_dir/theharvester/$target/theharvester_scan.txt" &
+  echo "TheHarvester is running in the background..."
 }
 
-# Main Script Execution
+function run_vulnerability_scanning() {
+  echo -e "\e[1;34mRunning vulnerability scanning...\e[0m"
+  nuclei -u "$target" -o "$output_dir/nuclei/$target/nuclei_scan.txt" &
+  echo "Nuclei scan is running in the background..."
+}
+
+function generate_report() {
+  echo -e "\e[1;34mGenerating report...\e[0m"
+  # TODO: Implement report generation (e.g., using a tool like 'markdown-report')
+  echo "Report generation is not yet implemented."
+}
+
+# --- Main Script Execution ---
+
 banner
 check_root
 update_system
 check_tools
 get_target
 create_directories
-run_scans
+recon_level=$(get_recon_level)
+
+run_subdomain_enumeration
+run_nmap_scan
+run_masscan_scan
+run_dns_enumeration
+run_fierce_scan
+run_web_server_analysis
+run_directory_bruteforcing
+run_wpscan
+run_theharvester
+run_vulnerability_scanning
+
+# Wait for background processes to finish
+wait
+
+generate_report
